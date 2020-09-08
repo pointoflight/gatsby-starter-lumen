d36bb8ebacf356b0be22348062d84ef4335a6602
Vedant Chavda <f20180209@goa.bits-pilani.ac.in>
2020-09-08 21:13:00 +0530

    update dome info

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
+  url: 'https://www.vedantc.com/',
   pathPrefix: '/',
-  title: 'Blog by John Doe',
+  title: 'vedantc.com',
   subtitle: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
   copyright: '© All rights reserved.',
   disqusShortname: '',
@@ -25,7 +25,7 @@ module.exports = {
     }
   ],
   author: {
-    name: 'John Doe',
+    name: 'Vedant Chavda',
     photo: '/photo.jpg',
     bio: 'Pellentesque odio nisi, euismod in, pharetra a, ultricies in, diam. Sed arcu.',
     contacts: {
